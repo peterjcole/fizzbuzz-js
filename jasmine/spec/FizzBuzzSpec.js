1804710d41fb814c55495b34a64304aff0b111cb
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-06-11 13:58:57 +0100

    complete

--- jasmine/spec/FizzBuzzSpec.js
+++ jasmine/spec/FizzBuzzSpec.js
@@ -18,6 +18,26 @@ describe(fizzBuzz, function() {
   })
 
   it ('should return 4 when passed the number 4', function() {
-    expect(fizzBuzz(4).toEqual('4'))
+    expect(fizzBuzz(4)).toEqual(4)
+  })
+
+  it ('should return fizz when passed the number 6', function() {
+    expect(fizzBuzz(6)).toEqual('fizz')
+  })
+
+  it ('should return buzz when passed the number 10', function() {
+    expect(fizzBuzz(10)).toEqual('buzz')
+  })
+
+  it ('should return buzz when passed the number 10', function() {
+    expect(fizzBuzz(10)).toEqual('buzz')
+  })
+
+  it ('should return fizzbuzz when passed the number 30', function() {
+    expect(fizzBuzz(30)).toEqual('fizzbuzz')
+  })
+
+  it('should return 7 when passed the number 7', function() {
+    expect(fizzBuzz(7)).toEqual(7)
   })
 })
--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,5 +1,6 @@
 function fizzBuzz(number) {
-  if (number === 15) return 'fizzbuzz' 
-  if (number === 5) return 'buzz'
-  return 'fizz'
+  if (number % 5 === 0 && number % 3 === 0) return 'fizzbuzz' 
+  if (number % 3 === 0) return 'fizz'
+  if (number % 5 === 0) return 'buzz'
+  return numbergoit 
 }
